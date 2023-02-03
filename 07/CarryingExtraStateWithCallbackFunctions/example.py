7d44bb151df3a26dba0cd03ce02caf69c2a4fd86
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-03 15:55:16 +0100

    Carry extra state using partial function application

--- 07/CarryingExtraStateWithCallbackFunctions/example.py
+++ 07/CarryingExtraStateWithCallbackFunctions/example.py
@@ -4,6 +4,7 @@
 # control of a callback.
 
 from typing import Any, Callable, Sequence
+from functools import partial
 
 
 def apply_async(fun:Callable, args:Sequence, *, callback:Callable) -> None:
@@ -41,26 +42,62 @@ def make_handler():
     return handler
 
 
+def make_coroutine():
+    sequence = 0
+    while True:
+        result = yield
+        sequence += 1
+        print(f"[{sequence}] Result: {result}")
+
+
+class SequenceNo:
+    def __init__(self):
+        self.sequence = 0
+
+
+def seq_handler(result, seq:SequenceNo):
+    seq.sequence += 1
+    print(f"[{seq.sequence}] Result: {result}")
+
+
 def main():
     # -*- (a) A simple callback example -*-
     print("-*------------------*-")
     print("-*- Simple Example -*-")
     print("-*------------------*-")
     apply_async(add, (2, 3), callback=print_result)
-    apply_async(add, ("hello", " world"), callback=print_result)
+    apply_async(add, ("hello", " simple example"), callback=print_result)
     print("-*--------------------------*-")
     print("-*- Using a bounded-method -*-")
     print("-*--------------------------*-")
     result = ResultHandler()
     apply_async(add, (2, 3), callback=result.handler)
-    apply_async(add, ("hello", " world"), callback=result.handler)
+    apply_async(add, ("hello", " bounded-metho"), callback=result.handler)
     # -*- (c) Using a closure
     print("-*-------------------*-")
     print("-*- Using a closure -*-")
     print("-*-------------------*-")
     handler = make_handler()
     apply_async(add, (2, 3), callback=handler)
-    apply_async(add, ("hello", " world"), callback=handler)
+    apply_async(add, ("hello", " closure"), callback=handler)
+    # -*- (d) Using a coroutine
+    print("-*---------------------*-")
+    print("-*- Using a coroutine -*-")
+    print("-*---------------------*-")
+    coro = make_coroutine()
+    next(coro)
+    apply_async(add, (2, 3), callback=coro.send)
+    apply_async(add, ("hello", " coroutine"), callback=coro.send)
+    # -*- (e) Partial function application -*-
+    print("-*--------------------------------------*-")
+    print("-*- Using partial function application -*-")
+    print("-*--------------------------------------*-")
+    seq = SequenceNo()
+    apply_async(add, (2, 3), callback=partial(seq_handler, seq=seq))
+    apply_async(
+        add, ("hello", " partial function application"),
+        callback=partial(seq_handler, seq=seq)
+    )
 
 
 if __name__ == "__main__":
