a240c749a80f83b180bef8a0f7cb153fd2f8afdd
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-03 21:48:35 +0100

    Extending classes with mixins: LoggedDict example

---
+++ 08/ExtendingClassesWithMixins/example1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+class LoggedMappingMixin:
+    """Add logging to get/set/delete operations for debugging."""
+    __slots__ = ()
+
+    def __getitem__(self, key):
+        print(f"Getting {str(key)}")
+        return super().__getitem__(key)
+    
+    def __setitem__(self, key, value):
+        print(f"Setting {key} = {value!r}")
+        return super().__setitem__(key, value)
+    
+    def __delitem__(self, key):
+        print(f"Deleting {str(key)}")
+        return super().__delitem__(key)
+    
+
+class SetOnceMappingMixin:
+    """Only allow a key to be set once."""
+    __slots__ = ()
+
+    def __setitem__(self, key, value):
+        if key in self:
+            raise KeyError(f"{str(key)} already set")
+        return super().__setitem__(key, value)
+    
+
+class StringKeysMappingMixin:
+    """Restrict keys to strings only."""
+    __slots__ = ()
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, str):
+            raise TypeError("keys must be strings")
+        return super().__setitem__(key, value)
+
+
+def main():
+    """Main entry."""
+    print("-*----------------------*-")
+    print("-*- LoggedDict Example -*-")
+    print("-*----------------------*-")
+
+    class LoggedDict(LoggedMappingMixin, dict):
+        pass
+
+    dct = LoggedDict()
+    dct["x"] = 23
+    print(dct["x"])
+    del dct["x"]
+
+
+if __name__ == "__main__":
+    main()
