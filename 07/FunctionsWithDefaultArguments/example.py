a25a0e9c2aad23513a2cf94905d5e42da8f443f5
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-03 16:21:58 +0100

    Example of better alternative for mutable defaults argument to functions)

--- 07/FunctionsWithDefaultArguments/example.py
+++ 07/FunctionsWithDefaultArguments/example.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 # Examples of a function with default arguments
 
+from typing import List, Optional
+
+
 def spam(b=[]):
     # -*- (a) Dangers of using a mutable default argument -*-
+    return b
+
+
+def better_spam(b:Optional[List]=None):
+    if b is None:
+        b = []
     return b
 
 
@@ -14,6 +23,14 @@ def main():
     a.append(2)
     b = spam()
     print(b)        # -*- Carfully observe result
+    print('-'*10)
+    # -*-
+    a = better_spam()
+    print(a)
+    a.append(1)
+    a.append(2)
+    b = better_spam()
+    print(b)
     print('-'*10)
 
 
