26d4a3a4ceeedce3b32b809dfde81bb1f6df1cc8
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-02-04 18:01:42 +0100

    Create a basic rest-based interface

--- 11/CreatingASimpleRestBasedInterface/server.py
+++ 11/CreatingASimpleRestBasedInterface/server.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import cgi
+import sys
+import time
+import signal
+from wsgiref.simple_server import make_server
 
+def sighandler(num, arg):
+    if num == signal.SIGINT:
+        sys.exit(0)
 
 def notfound_404(env, startResponse):
     startResponse(
@@ -25,3 +32,57 @@ class PathDispatcher:
     def register(self, method, path, fun):
         self.pathmap[method.lower(), path] = fun
         return fun
+
+
+_helloResponse = """\
+<html>
+    <head>
+        <title>Hello {name}</title>
+    </head>
+    <body>
+        <h1>Hello {name}!</h1>
+    </body>
+</html>"""
+
+
+def hello_world(env, startResponse):
+    startResponse("200 OK", [("Content-type", "text/html")])
+    params = env["params"]
+    response = _helloResponse.format(name=params.get("name"))
+    yield response.encode("utf-8")
+
+
+_localtimeResponse = """\
+<?xml version="1.0"?>
+<time>
+    <year>{t.tm_year}</year>
+    <month>{t.tm_mon}</month>
+    <day>{t.tm_mday}</day>
+    <hour>{t.tm_hour}</hour>
+    <minute>{t.tm_min}</minute>
+    <second>{t.tm_sec}</second>
+</time>"""
+
+
+def localtime(env, startResponse):
+    startResponse("200 OK", [("Content-type", "application/xml")])
+    response = _localtimeResponse.format(t=time.localtime())
+    yield response.encode("utf-8")
+
+
+def main():
+    """Main entry."""
+    signal.signal(signal.SIGINT, sighandler)
+    # -*- Create the path dispatcher and register functions
+    dispatcher = PathDispatcher()
+    dispatcher.register("GET", "/hello", hello_world)
+    dispatcher.register("GET", "/localtime", localtime)
+
+    # -*- Launch a basic server
+    httpd = make_server("", 8080, dispatcher)
+    print("Serving on port 8080...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
